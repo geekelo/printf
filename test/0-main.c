4007bab0c10eb5514387294c03b1adddac4d106c
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-03-27 14:30:56 +0100

    made modificationsw

--- test/0-main.c
+++ test/0-main.c
@@ -1,6 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
-#include "../main.h"
+#include "main.h"
 
 /**
  * main - Entry point
@@ -38,8 +38,8 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-/*    _printf("Unknown:[%r]\n");
-   printf("Unknown:[%r]\n"); 
-*/  return (0);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    return (0);
 }
 
